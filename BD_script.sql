f71cae44225bd7d721ab71bf723a00b14bd674d6
Esdras Silva de Lima Junior <estudos14j@gmail.com>
2023-12-20 23:29:43 -0300

    update

--- BD_script.sql
+++ BD_script.sql
@@ -1,23 +1,13 @@
 CREATE SCHEMA atv_faculdade2;
 USE atv_faculdade2;
 
-CREATE TABLE aluno(
-	ra INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE funcionario(
+	matricula INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(40) NOT NULL,
     email VARCHAR(40) NOT NULL,
+    cpf varchar(11),
     telefone INT,
-    fk_curso INT
+    cargo varchar(25)
 );
 
-CREATE TABLE cursos(
-	id_curso INT PRIMARY KEY AUTO_INCREMENT,
-	nomeCurso VARCHAR(40) NOT NULL UNIQUE,
-    acronimo_cur VARCHAR(5) NOT NULL UNIQUE,
-    duracao VARCHAR(15) NOT NULL
-);
-
-ALTER TABLE aluno ADD CONSTRAINT fk_cursos FOREIGN KEY(fk_curso) REFERENCES cursos(id_curso);
-SELECT * FROM cursos;
-INSERT INTO cursos(nomeCurso, acronimo_cur, duracao) VALUES('Ciencias da Computação', 'CC', '8 Semestres');
-INSERT INTO cursos(nomeCurso, acronimo_cur, duracao) VALUES('Sistemas da Informação', 'SI', '8 Semestres');
-INSERT INTO cursos(nomeCurso, acronimo_cur, duracao) VALUES('Analise e Desenvolvimento de Sistemas', 'ADS', '4 Semestres');
+SELECT * FROM funcionario;
